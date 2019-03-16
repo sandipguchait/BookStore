4e9ea0be93fe0211e3a4c5c1de0e2bb4bad29cc3
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-16 19:22:10 +0530

    making a Post request to add a new book and storing it to database

--- server/server.js
+++ server/server.js
@@ -16,11 +16,26 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+//GET request
+
+//POST request
+app.post('/book/new', (req, res)=> {
+    const book = new Book(req.body)
+    book.save((err, doc)=>{
+        if(err) return res.status(400).send(err);
+        res.status(200).json({
+            post: true,
+            bookId: doc._id
+        })
+    })
+})
+//UPDATE request
+
+//DELETE request
 
 
 
 const port = process.env.PORT || 3001
-
 app.listen(port, ()=> {
     console.log(`SERVER Running on ${port}`)
 })
