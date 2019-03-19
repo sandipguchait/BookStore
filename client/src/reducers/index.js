8a535b8165c43b07c64f678260b387a5718bcb3f
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-19 17:35:54 +0530

    adding reducers to combine reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import books from './bookreducer';
+import user  from './userreducer';
+
 
  const rootReducer = combineReducers({
-
+    books,
+    user
 });
 
 export default rootReducer;
