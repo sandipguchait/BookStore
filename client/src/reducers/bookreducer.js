76e8e7ea3a69a960efa70207065f8441db810742
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-19 23:53:27 +0530

    making book reducer to fetch all books

--- client/src/reducers/bookreducer.js
+++ client/src/reducers/bookreducer.js
@@ -1,6 +1,12 @@
-export default function( state={}, action ){
-    switch(action.type){
 
+const INITIAL_STATE = {
+    bookList : null
+}
+
+export default function( state= INITIAL_STATE, action ){
+    switch(action.type){
+        case 'GET_BOOKS':
+            return { ...state, bookList:action.payload}
         default:
             return state;
     }
