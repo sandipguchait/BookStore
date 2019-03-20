72a4c8ba8ee30a5bdb98669f980a460a433210e5
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-20 18:20:25 +0530

    Building the UI of the Home component

--- client/src/WIDGETSUI/bookitem.js
+++ client/src/WIDGETSUI/bookitem.js
@@ -8,6 +8,18 @@ const BookItems = (item) => {
             <div className="book_header">
                 <h2>{item.name}</h2>
             </div>
+            <div className="book_items">
+                <div className="book_author">{item.author}</div>
+                <div className="book_bubble">
+                    <strong>Price</strong> ${item.price}
+                </div>
+                <div className="book_bubble">
+                    <strong>Page</strong> {item.pages}
+                </div>
+                <div className="book_bubble rating">
+                    <strong>Rating</strong> {item.rating}
+                </div>
+            </div>
         </Link>
     );
 };
